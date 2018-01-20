0ca406746eb78a762b150050c00bf600c7988de7
anuous <anuous@aliyun.com>
2018-01-20 12:58:39 +0800

    mybatis 注解集成 and test

---
+++ src/main/java/com/example/demo/daoImpl/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.daoImpl;
+
+
+import com.example.demo.model.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component(value="userMapper")
+public interface UserMapper {
+
+    @Insert("insert into user(t_name,t_age) values(#{name},#{age})")
+    public int insert(@Param("name") String name,@Param("age")Integer age);
+
+    @Results({
+            @Result(property = "name",column = "t_name"),
+            @Result(property = "id",column = "t_id"),
+            @Result(property = "age",column = "t_age"),
+    })
+    @Select("select * from user where t_name = #{name}")
+    public User getByName(@Param("name") String name);
+
+}
